bee8e27914af6ac918b9f70fa5eaa17367521384
Nikolay Vasilchuk <nikolay.vasilchuk@corp.mail.ru>
2018-07-03 13:24:08 +0300

    Password fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ PASSWORD_ENCODED="$(mkpasswd -m md5 <<< $PASSWORD)"
 if [ "$1" = "start_proxy" ]; then
     cp 3proxy.cfg.dist 3proxy.cfg
     sed -i 's/__USER__/'"$USER"'/g' 3proxy.cfg && \
-    sed -i 's/__PASSWORD__/'"$PASSWORD_ENCODED"'/g' 3proxy.cfg && \
+    sed -i 's#__PASSWORD__#'"$PASSWORD_ENCODED"'#g' 3proxy.cfg && \
     sed -i 's/__PORT__/'"$PORT"'/g' 3proxy.cfg && \
     sed -i 's/__DNS1__/'"$DNS1"'/g' 3proxy.cfg && \
     sed -i 's/__DNS2__/'"$DNS2"'/g' 3proxy.cfg && \
